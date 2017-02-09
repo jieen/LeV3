d6878ade52ba463ff6c961c46b0bdaaa0161608b
luorenyang <luorenyang@gobuylight.com>
2017-02-09 17:10:11 +0800

    init Lettin V3

---
+++ LettinApp/src/main/java/cn/lettin/lettinv3/MainActivity.java
@@ -0,0 +1,21 @@
+package cn.lettin.lettinv3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import cn.lettin.lettinlibs.LettinNetwork;
+
+public class MainActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView tv = (TextView) findViewById(R.id.tv);
+
+        LettinNetwork ln = new LettinNetwork();
+        tv.setText(ln.stringFromJNI()+" lebo Version:"+ln.getGwVersion());
+    }
+}
